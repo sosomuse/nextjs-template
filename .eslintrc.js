0dd6d63bf75e7e41f39e9da5398e583414e8b72f
So Nishimura <s.nishimura@gemcook.com>
2022-10-02 21:26:14 +0900

    feat: fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,17 @@
-{
-  "extends": [
+module.exports = {
+  extends: [
     "next/core-web-vitals",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
     "plugin:import/recommended",
     "plugin:import/warnings",
-    "prettier"
+    "prettier",
   ],
-  "rules": {
+  parserOptions: {
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
     "import/order": [
       "error",
       {
@@ -15,4 +21,4 @@
       }
     ]
   }
-}
+};
